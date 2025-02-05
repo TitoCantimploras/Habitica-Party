02ce46d8031efca114b246eaa2fba5e188490ac6
Delta-Water <3627371741@qq.com>
2025-02-05 20:38:37 +0800

    Update update_daily_description.py

--- scripts/update_daily_description.py
+++ scripts/update_daily_description.py
@@ -14,15 +14,7 @@ def update_habitica_description(content, translation):
         "x-api-key": os.environ["HABITICA_API_KEY"],
         "Content-Type": "application/json"
     }
-    description = f"""
-### 每日一言 · Daily Sentence 🌹
-    
-{content}
-    
-{translation}
-    
-#### Want to learn more about the party's purpose, rules, and other information? [Click here!](https://github.com/Delta-Water/Habitica-Party/blob/main/party_description.md)
-    """
+    description = f"### 每日一言 · Daily Sentence 🌹\n\n{content}\n\n{translation}\n\n#### Want to learn more about the party's purpose, rules, and other information? [Click here!](https://github.com/Delta-Water/Habitica-Party/blob/main/party_description.md)"
     data = {"description": description}
     
     response = requests.put(url, headers=headers, data=json.dumps(data))
