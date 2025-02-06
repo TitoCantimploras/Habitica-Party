33740242cceec6560c3eee665ca8b9ab14c7cf8d
Delta-Water <3627371741@qq.com>
2025-02-07 00:21:36 +0800

    Update update_description.py

--- scripts/update_description.py
+++ scripts/update_description.py
@@ -10,7 +10,7 @@ import time
 logger = logging.getLogger('habitica_update_description')
 logger.setLevel(logging.DEBUG)
 
-handler = RotatingFileHandler('log/update_description.log', maxBytes=10*1024, backupCount=1)
+handler = RotatingFileHandler('log/update_description.log', maxBytes=1024*1024, backupCount=1)
 handler.setLevel(logging.DEBUG)
 handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
