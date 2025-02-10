b988a9f16b4e4debd1808a4f8945acfc6ea7430e
Delta-Water <3627371741@qq.com>
2025-02-10 09:36:22 +0800

    Update manage_members.py

--- scripts/manage_members.py
+++ scripts/manage_members.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.DEBUG)
 
 handler = RotatingFileHandler('logs/manage_members.log', maxBytes=1024*1024, backupCount=1)
 handler.setLevel(logging.DEBUG)
-handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
